e20c8a73038c2b9e9b471d8a45451385e747dcb3
ljelschen <mail@lars-jelschen.de>
2022-02-24 21:03:22 +0100

    Bremen X Sender
    lädt alle Bremen ... Sender in die Datenbank

--- backend/getSongs.py
+++ backend/getSongs.py
@@ -1,33 +1,33 @@
+#import data
 from bs4 import BeautifulSoup
 import requests
 import json
 import datetime
-
+import time
 #MySQL
 from pypika import Query, Table, Field
 from mysqlsetup import connectToMySQL
 mydb, cursor = connectToMySQL()
 
-
+#set the date to today
 date = datetime.date.today()
 
+#select the songs tabe from the database
 tbSongs = Table('songs')
 
-
-
-
+# read all songs from the database
 def getAllSongs():
     q = Query.from_(tbSongs).select('*').orderby(tbSongs.time)
     q = str(q).replace('"', '`')
     cursor.execute(q)
     return cursor.fetchall()
 
+#check for duplicates songs in the database
 def checkDuplicateSong(song):
-    q = Query.from_(tbSongs).select('*').where(tbSongs.date == date).where(tbSongs.time == song['time']).where(tbSongs.interpret == song['interpret']).where(tbSongs.title == song['title'])
+    q = Query.from_(tbSongs).select('*').where(tbSongs.date == date).where(tbSongs.date == song['date']).where(tbSongs.time == song['time']).where(tbSongs.interpret == song['interpret']).where(tbSongs.title == song['title'])
     q = str(q).replace('"', '`')
     cursor.execute(q)
     result = cursor.fetchall()
-  
     if len(result) > 0:
         #return true if song is already in database
         return True
@@ -35,24 +35,77 @@ def checkDuplicateSong(song):
         #return fals if song is not in the  database
         return False
 
-def saveSongsByTimeBremenX(station, searchTime, url, debug=False):
-    #This does Work for Bremen 1, Bremen 2, Bremen 4, Bremen Next
+
+
+
+#### Bremen 4
+def saveSongsBremen4(station, url, debug=False):
+    #This does Work for Bremen 4
     r = requests.get(url)
     if r.status_code == 200:
+        songs = []
         soup = BeautifulSoup(r.content,"html.parser")
         for song in soup.find_all('tr')[1::]:
-            time = song.find_all('td')[0].text.replace("\n","")
+            timeNow = song.find_all('td')[0].text.replace("\n","")
             interpret = song.find_all('td')[1].text.replace("\n","")
             title = song.find_all('td')[2].text.replace("\n","")
-            print(checkDuplicateSong({'time': time, 'interpret': interpret, 'title': title}))
-            if not debug and not checkDuplicateSong({'time': time, 'interpret': interpret, 'title': title}):
-                q = Query.into(tbSongs).columns('date', 'time', 'station', 'interpret', 'title').insert(date, time, station, interpret, title)
+            #print(checkDuplicateSong({'time': time, 'interpret': interpret, 'title': title}))
+            if not debug and not checkDuplicateSong({'date': date, 'time': timeNow, 'interpret': interpret, 'title': title}):
+                q = Query.into(tbSongs).columns('date', 'time', 'station', 'interpret', 'title').insert(date, timeNow, station, interpret, title)
                 q = str(q).replace('"', '`')
                 cursor.execute(q)
                 mydb.commit()
+            songs.append({'date': date, 'time': timeNow, 'interpret': interpret, 'title': title})
+        return songs
     else:
         return None
 
+#reads the songs from the website via json and saves them to the database
+def saveSongsBremenX(station, url):
+    #This does Work for Bremen 1, Bremen 2, Bremen 4, Bremen Next
+    result = requests.get(url)
+    if result.status_code == 200:
+        #convert to json
+        json_data = json.loads(result.text)
+        #get the songs
+        songsRetrun = []
+        songs = json_data['playlistPrevious']
 
-url = "https://www.bremenvier.de/titelsuche-102~ajax.html?playlistsearch-searchDate=2022-02-24&playlistsearch-searchTime=18:10&_=1645722701763"
-songs = saveSongsByTimeBremenX("Bremen 4", "18:10", url)
+        for song in songs:
+            state = checkDuplicateSong({'date': date, 'time': song['time'], 'interpret': song['artist'], 'title': song['song']})
+            if not state:
+                q = Query.into(tbSongs).columns('date', 'time', 'station', 'interpret', 'title').insert(date, song['time'], station, song['artist'], song['song'])
+                q = str(q).replace('"', '`')
+                cursor.execute(q)
+                mydb.commit()
+            songsRetrun.append({'date': date, 'time': song['time'], 'interpret': song['artist'], 'title': song['song']})
+        return songsRetrun
+    else:
+        return None
+    
+
+##### Json Abfrage
+timestamp = str(int(round(time.time(), 0))) + '000'
+
+#Bremen 1
+urlBremen1 = f"https://www.bremeneins.de/startseite-bremen-eins-100~ajax_ajaxType-epg.json?_={timestamp}"
+saveSongsBremenX("Bremen 1", urlBremen1)
+
+#Bremen 2
+urlBremen2 = f"https://www.bremenzwei.de/startseite-bremen-zwei-100~ajax_ajaxType-epg.json?_={timestamp}"
+saveSongsBremenX("Bremen 2", urlBremen2)
+
+#Bremen Next
+urlBremenNext = f"https://www.bremennext.de/bremennext-startseite100~ajax_ajaxType-epg.json?_={timestamp}"
+saveSongsBremenX("Bremen Next", urlBremenNext)
+
+##### Speziale Abfrage
+
+#Bremen 4
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+urlBremen4 = f"https://www.bremenvier.de/titelsuche-102~ajax.html?playlistsearch-searchDate={date}&playlistsearch-searchTime={hour}%3A{minute}"
+saveSongsBremen4("Bremen 4", urlBremen4)
+
+
+
