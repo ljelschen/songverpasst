2cb2a57ac1f4e903d8818266f11282334bfe7a21
ljelschen <mail@lars-jelschen.de>
2022-03-01 20:32:46 +0100

    zweite Tabelle in Datenbank und song Seite angepasst

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -32,6 +32,9 @@
     font-size: 1.25rem;
     font-weight: 400;
   }
+
+
+
 </style>
 <template>
   <div class="header ">
@@ -53,8 +56,50 @@
     </svg>
   </div>
 
+<section id="section-bulbs" class="d-flex align-items-center position-relative bg-dark bg-size-cover bg-position-center min-vh-100 overflow-hidden pt-6 pb-lg-5" >
+        <div class="container pt-4 pb-5 py-lg-5">
+          <div class="row align-items-center py-3">
+            <div class="col-xl-6 col-lg-5 d-flex justify-content-end">
+              <div class="pt-2 mx-auto mb-5 mb-lg-0 ms-lg-0 me-xl-7 text-center text-lg-start" style="max-width: 495px;">
+                <h1 class="display-4 text-light pb-2"><span class="fw-light">Have a look </span>Around!</h1>
+                <p class="h4 fw-light text-light opacity-70 line-height-base">And you will find everything you need to build a great looking website.</p><a class="d-inline-flex align-items-center text-decoration-none pt-2 mt-4 mb-5" href="#demos" data-scroll=""><span class="btn btn-icon rounded-circle border-light flex-shrink-0 px-3"><i class="ai-arrow-down h4 text-light my-1"></i></span><span class="ms-3 text-light fw-medium">View Demos</span></a>
+                <hr class="hr-light mt-0 mb-5">
+                <div class="row">
+                  <div class="col-sm-4 mb-4 mb-sm-0">
+                    <div class="h1 text-light mb-1">14</div>
+                    <div class="h5 text-light fw-normal opacity-70 mb-2">Demo Homepages</div><span class="badge rounded-pill bg-success">More coming</span>
+                  </div>
+                  <div class="col-sm-4 mb-4 mb-sm-0">
+                    <div class="h1 text-light mb-1">50+</div>
+                    <div class="h5 text-light fw-normal opacity-70 mb-1">Flexible Components</div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="h1 text-light mb-1">47</div>
+                    <div class="h5 text-light fw-normal opacity-70 mb-1">Inner Page Templates</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-7">
+              <div id="scene" sclass="parallax ms-lg-n5" style="max-width: 920px; transform: translate3d(0px, 0px, 0px) rotate(0.0001deg); transform-style: preserve-3d; backface-visibility: hidden; pointer-events: none;">
+                <div class="parallax-layer position-relative" data-depth="0.1" style="transform: translate3d(-6.9px, 1.9px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: relative; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer01.png" alt="Layer"></div>
+                <div class="parallax-layer" style="z-index: 2; transform: translate3d(-20.7px, 5.7px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;" data-depth="0.3"><img src="img/demo/presentation/intro/layer02.png" alt="Layer"></div>
+                <div class="parallax-layer" data-depth="0.2" style="transform: translate3d(-13.8px, 3.8px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer03.png" alt="Layer"></div>
+                <div class="parallax-layer" style="z-index: 3; transform: translate3d(-6.9px, 1.9px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;" data-depth="0.1"><img src="img/demo/presentation/intro/layer04.png" alt="Layer"></div>
+                <div class="parallax-layer" data-depth="0.15" style="transform: translate3d(-10.3px, 2.9px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer05.png" alt="Layer"></div>
+                <div class="parallax-layer" style="z-index: 4; transform: translate3d(-17.2px, 4.8px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;" data-depth="0.25"><img src="img/demo/presentation/intro/layer06.png" alt="Layer"></div>
+                <div class="parallax-layer" data-depth="0.3" style="transform: translate3d(-20.7px, 5.7px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer07.png" alt="Layer"></div>
+                <div class="parallax-layer" data-depth="0.4" style="transform: translate3d(-27.6px, 7.6px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer08.png" alt="Layer"></div>
+                <div class="parallax-layer" data-depth="0.35" style="transform: translate3d(-24.1px, 6.7px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><img src="img/demo/presentation/intro/layer09.png" alt="Layer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
 </template>
 
 <script>
+
 
 </script><!-- Add "scoped" attribute to limit CSS to this component only -->
